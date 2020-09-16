1f0e682fbbee0bc6c65c51d937cf3f677efbc2f6
Sergey Glazov <yaglazov@gmail.com>
2020-09-16 16:24:28 +0300

    inc

--- docs/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- *
- */
-return [
-    'url' => 'https://ed-link.ru/',
-];
---
+++ docs/php/config.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ *
+ */
+return [
+    'url' => 'http://ed-link.ru/',
+    'css' => filemtime( $_SERVER['DOCUMENT_ROOT'] . "/assets/styles/css.css" )
+];
